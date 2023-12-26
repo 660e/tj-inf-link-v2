64c32a00b100bf231454e5bbc2ca57b0fbed2ab9
Min <solunar@vip.qq.com>
2023-12-26 15:11:17 +0800

    feat: 系统管理-资源空间管理-新增/编辑增加参数

--- src/pages/system/resource/dialogs/create.vue
+++ src/pages/system/resource/dialogs/create.vue
@@ -2,6 +2,17 @@
   <iot-dialog :visible="visible" :width="500" :title="isCreate ? '新增' : '修改'" @confirm="confirm" @cancel="cancel">
     <q-form>
       <iot-form-item :value.sync="forms.resSpaceName" :validator="$v.forms.resSpaceName" label="名称" type="text" width="70" maxlength="32" />
+      <iot-form-item
+        v-if="tenantIdOptions.length"
+        label="租户"
+        :value.sync="forms.tenantId"
+        :validator="$v.forms.tenantId"
+        :options="tenantIdOptions"
+        option-label="tenantName"
+        option-value="id"
+        type="select"
+        width="70"
+      />
       <iot-form-item :value.sync="forms.resSpaceDescription" label="描述" type="textarea" width="70" maxlength="100" />
     </q-form>
   </iot-dialog>
@@ -9,8 +20,8 @@
 
 <script>
 import { required } from 'vuelidate/lib/validators';
+import { extend, SessionStorage } from 'quasar';
 import { sysApi } from '@/api/tdf-service-sys/sys.js';
-import { extend } from 'quasar';
 
 export default {
   data() {
@@ -22,13 +33,16 @@ export default {
       },
       forms: {
         resSpaceName: '',
+        tenantId: '',
         resSpaceDescription: ''
-      }
+      },
+      tenantIdOptions: []
     };
   },
   validations: {
     forms: {
-      resSpaceName: { required }
+      resSpaceName: { required },
+      tenantId: { required }
     }
   },
   created() {
@@ -40,7 +54,14 @@ export default {
       if (!this.isCreate) {
         this.forms = extend(true, {}, row);
       }
-      this.visible = true;
+      const p0 = sysApi.checkCurrUserIsSysAdmin(SessionStorage.getItem('account').login.name);
+      const p1 = sysApi.tenantlist();
+      Promise.all([p0, p1]).then(response => {
+        if (response[0]) {
+          this.tenantIdOptions = response[1];
+        }
+        this.visible = true;
+      });
     },
     confirm() {
       this.$v.$touch();
